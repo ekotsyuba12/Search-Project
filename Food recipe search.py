2d8a7f7d3a1e2c9dc808f82116ea7808ae482545
Рома Коцюба <romako@iMac.local>
2020-04-22 22:58:53 +0500

    First version

---
+++ Food recipe search.py
@@ -0,0 +1,21 @@
+import requests
+
+from pprint import pprint
+
+ingredient = input("What ingredient do you want to use for cooking? ")
+app_ID = 'e6c8d584'
+app_key = 'c05290c6cb02ff6af43654220f30dfb9'
+
+url = 'https://api.edamam.com/search?q={}&app_id={}&app_key={}'.format(ingredient, app_ID, app_key)
+
+response = requests.get(url)
+
+print (response)
+
+recipe = response.json()
+pprint(recipe)
+#recipe_hits = recipe['hits']
+#for hits in recipe_hits:
+    #hits['recipe']['url']
+
+#pprint(recipe['recipe_hits'])
